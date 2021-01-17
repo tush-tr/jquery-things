24eba40ccc65383fc4489ba2b834f023255569af
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2021-01-18 02:07:31 +0530

    Add a word starting from the showed character

--- list-words/script.js
+++ list-words/script.js
@@ -1,14 +1,8 @@
 var words = ["A","B","C","D","E","F","G","H","I","J","k","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
-
-$(".character").click(function(){
+$("button").click(function(){
     $(".character").text(words[[Math.floor(Math.random() * 26)]]);
     var c = $(".character").text();
     $(".input input").attr("placeholder",`Enter a word starting from ${c}`);
-
-})
-$("button").click(function(){
     $(".show").append(`${$(".input input").val()}<br>`);
-
     $(".input input").val(null);
-
 })
